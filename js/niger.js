14d132699c1a27c2d26b384ba99fd11f5589a44a
Mega Bebra <dubrovskipavel976@gmail.com>
2022-03-27 14:29:12 +0300

    bobr

--- js/niger.js
+++ js/niger.js
@@ -1,9 +1,54 @@
-let red=0
-let green=0
-let blue=0
-setInterval(()=>{
-  document.querySelector('html').style.background=`rgb(${red},${green},${blue})`
-red++
-  green++
-  blue++
-})
+// let red = 0
+// let green = 0
+// let blue = 0
+
+// setInterval(() => {
+//   if (red <= 255) {
+//     for (let i = 0; i <= 255; i++) {
+//       setTimeout(() => {
+//         red++
+//         document.querySelector("html").style.background = `rgb(${red},${green},${blue})`
+//       }, 50*i);
+//     }
+//   } else {
+//     for (let i = 0; i <= 255; i++) {
+      
+//       setTimeout(() => {
+//         red--
+//         document.querySelector("html").style.background = `rgb(${red},${green},${blue})`
+//       }, 50*i);
+//     }
+//   }
+//   if (blue <= 255) {
+//     for (let i = 0; i <= 255; i++) {
+//       setTimeout(() => {
+//         blue++
+//         document.querySelector("html").style.background = `rgb(${blue},${green},${blue})`
+//       }, 50*i);
+//     }
+//   } else {
+//     for (let i = 0; i <= 255; i++) {
+      
+//       setTimeout(() => {
+//         blue--
+//         document.querySelector("html").style.background = `rgb(${blue},${green},${blue})`
+//       }, 50*i);
+//     }
+//   }
+//   if (green <= 255) {
+//     for (let i = 0; i <= 255; i++) {
+//       setTimeout(() => {
+//         green++
+//         document.querySelector("html").style.background = `rgb(${green},${green},${blue})`
+//       }, 50*i);
+//     }
+//   } else {
+//     for (let i = 0; i <= 255; i++) {
+      
+//       setTimeout(() => {
+//         green--
+//         document.querySelector("html").style.background = `rgb(${green},${green},${blue})`
+//       }, 50*i);
+//     }
+//   }
+// }, 15000)
